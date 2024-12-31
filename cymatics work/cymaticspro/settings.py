3f23f234e781638e317a44f72aff292773b0b776
shanchana <shanchana.s2022ai-ml@sece.ac.in>
2024-12-31 17:05:35 +0530

    settings changed

--- cymatics work/cymaticspro/settings.py
+++ cymatics work/cymaticspro/settings.py
@@ -22,14 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
 
 # Retrieve the SECRET_KEY from environment variables
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
